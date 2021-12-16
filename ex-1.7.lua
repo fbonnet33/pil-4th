ccdcf85b8e355942b85550978a64657ed97a8663
Fabrice Bonnet <56681792+fbonnet33@users.noreply.github.com>
2021-12-16 12:02:18 +0100

    Update ex-1.7.lua
    Doing an exhautive test shows us that both expressions are identical
    
    In:
    x and y and not z or not y and x
    'not' has precedence
    
    ==>> 
    x and y and (not z) or (not y) and x
    
    Then 'and' has precedence
    ==>>
    (x and y and (not z)) or ((not y) and x)
    
    which is identical to the former expression

--- ex-1.7.lua
+++ ex-1.7.lua
@@ -5,13 +5,67 @@
 -- Are the parentheses necessary? Would you recommend their use in
 -- that expression?
 
--- They are not, because the precedence order of the logical operators
--- is not > and > or.  So the expression could simply be written
+-- They are, because the precedence order of the logical operators
+-- is not > and > or.  
 
-x and y
-  and not z
-or not y
-   and x
+-- to be sure, let's do an exhaustive test
+function with_parenthesis(x, y, z)
+   return (x and y and (not z)) or ((not y) and x)
+end
 
+
+function without_parenthesis(x, y, z)
+   return x and y and not z or not y and x
+end
+
+is_egal = true
+for _, x in ipairs({false, true}) do
+    for _, y in ipairs({false, true}) do
+        for _, z in ipairs({false, true}) do
+            if with_parenthesis(x,y,z) ~= without_parenthesis(x,y,z) then
+                print (x, y, z)
+                is_egal = false
+            end
+        end
+    end
+end
+
+print(string.format("with_parenthes and whitout_parenthesis are %s",
+                is_egal and "egals" or "not egals"))
 -- The use of parenthesis make the expression arguably more readable,
 -- though.
+
+
+
+
+-- Idem if we use 5 parameters ???
+function with_parenthesis2(x, y, z, u, t)
+   return (x and y and (not z)) or ((not u) and t)
+end
+
+
+function without_parenthesis2(x, y, z, u, t)
+   return x and y and not z or not u and t
+end
+
+is_egal = true
+for _, x in ipairs({false, true}) do
+    for _, y in ipairs({false, true}) do
+        for _, z in ipairs({false, true}) do
+            for _, u in ipairs({false, true}) do
+                for _, t in ipairs({false, true}) do
+                    if with_parenthesis2(x,y,z,u,t) ~= without_parenthesis2(x,y,z,u,t) then
+                        print (x, y, z, u, t)
+                        is_egal = false
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+print(string.format("with_parenthes2 and whitout_parenthesis2 are %s",
+                is_egal and "egals" or "not egals"))
+
+
