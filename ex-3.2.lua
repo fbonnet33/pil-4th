b668ad0aad318daf74dc1c1d3d2ad2b7273d14ae
Fabrice Bonnet <56681792+fbonnet33@users.noreply.github.com>
2021-12-16 17:37:33 +0100

    Update ex-3.2.lua
    Another solution

--- ex-3.2.lua
+++ ex-3.2.lua
@@ -14,14 +14,16 @@ math.maxinteger * 2                     --> -2
 math.mininteger * 2 - 2                 --> -2
 math.mininteger * 2                     --> 0
 
--- > math.maxinteger * math.maxinteger  --> 1
-
-math.maxinteger % math.mininteger                                         --> -1
-(math.maxinteger % math.mininteger) * (math.maxinteger % math.mininteger) --> 1
-math.maxinteger * math.maxinteger                                         --> 1
 
 -- > math.mininteger * math.mininteger  --> 0
+-- math.mininteger = 0x80...0
+-- multiplying by 0x80...0 is equivalent to a 63 left bit shift
+-- so math.mininteger * math.mininteger = 0x80...0 << 63 === 0 [2⁶⁴]
+ math.mininteger * math.mininteger  --> 0
 
-math.mininteger % math.mininteger                                         --> 0
-(math.mininteger % math.mininteger) * (math.mininteger % math.mininteger) --> 0
-math.mininteger * math.mininteger                                         --> 0
+-- > math.maxinteger * math.maxinteger  --> 1
+-- math.maxinteger * math.maxinteger = (math.mininteger - 1)²
+--  = math.mininteger² - 2*math.mininteger + 1
+--  = 0 - 0 + 2
+--  = 1
+math.maxinteger * math.maxinteger  --> 1
